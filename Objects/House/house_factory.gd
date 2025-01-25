26876f7bbea8c592995a5952e4c165315e204871
CWmain <cameronwhite2478@gmail.com>
2025-01-26 00:51:20 +1100

    Created a house factory to automatically place 25 evenly placed houses, if I end up adding other building types I could consider randomly selecting a building type with different demands

---
+++ Objects/House/house_factory.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+## Generates "toGenerate" number of houses of row length "rowLength"
+@export var rowLength: int = 1
+@export var toGenerate: int = 1
+
+## The width gap between houses
+@export var HOUSE_WIDTH: int = 32
+## The height gap between houses
+@export var HOUSE_HEIGHT: int = 32
+
+var houseToPlace = preload("res://Objects/House/house.tscn")
+var toPlacePosition: Vector2 = Vector2.ZERO 
+
+func _ready():
+	var i : int = 0
+	while i < toGenerate:
+		var curHouse: House = houseToPlace.instantiate()
+		curHouse.position = toPlacePosition
+		add_child(curHouse)
+		
+		toPlacePosition += Vector2(HOUSE_WIDTH, 0)
+		# Max row sized reach, so reset to new row
+		if (toPlacePosition[0] > HOUSE_WIDTH*(rowLength-1)):
+			toPlacePosition[0] = 0
+			toPlacePosition[1] += HOUSE_HEIGHT
+		i += 1
+		
