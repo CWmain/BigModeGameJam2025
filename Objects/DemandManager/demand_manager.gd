a2a3b6ba2720c242af59b49b312b2ebde510561d
CWmain <cameronwhite2478@gmail.com>
2025-01-26 15:05:33 +1100

    Refactored LevelTimer to use updateDemand and updateSupply instead of tick. The demand display gets its update signal from demand manger to change the displayed value. The Total demand is stored in DemandManger

--- Objects/DemandManager/demand_manager.gd
+++ Objects/DemandManager/demand_manager.gd
@@ -1,6 +1,14 @@
 extends Node
+class_name DemandManager
 
+@export var timer: LevelTimer 
+signal updateLabel
 
+var totalDemand: int = 0
+
+func _ready():
+	assert(timer != null)
+	timer.updateDemand.connect(_on_updateDemand)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
@@ -13,3 +21,12 @@ func toggleAllHouses():
 	for d: House in demanaders:
 		d.isDemanding = !(d.isDemanding)
 
+func _on_updateDemand():
+	var curDemand: int = 0
+	var demanaders = get_tree().get_nodes_in_group("Demander")
+	for d: House in demanaders:
+		if (d.isDemanding):
+			curDemand += d.DEMAND
+	totalDemand = curDemand
+	updateLabel.emit()	
+	
--- Objects/LevelTimer/LevelTimer.gd
+++ Objects/LevelTimer/LevelTimer.gd
@@ -1,7 +1,14 @@
 extends Timer
 class_name LevelTimer
 
+# Consider having different ticks for different systems if lag becomes an issue 
+signal updateDemand
+signal updateSupply
+
 signal tick
 
 func _on_timeout():
+	updateDemand.emit()
+	updateSupply.emit()
+
 	tick.emit()
