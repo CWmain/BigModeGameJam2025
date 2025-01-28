95a32f8ec0937e9876a9881b57492c4ab6a374d3
CWmain <cameronwhite2478@gmail.com>
2025-01-28 14:33:43 +1100

    Added exports and better variable names

--- Objects/CoalStuff/coal_dispenser.gd
+++ Objects/CoalStuff/coal_dispenser.gd
@@ -1,12 +1,14 @@
 extends Node2D
 
+@export var MAX_COAL: int = 5
+@export var LAUNCH_SPEED: int = 500
 var coal = preload("res://Objects/CoalStuff/coal.tscn")
 
 func _physics_process(_delta):
 	var coals = get_tree().get_nodes_in_group("Coal")
 
 	if coals.size() < 5:
-		var c : RigidBody2D = coal.instantiate()
-		c.transform = transform
-		c.apply_impulse(Vector2(500,0))
-		get_tree().get_root().add_child(c)
+		var newCoal : RigidBody2D = coal.instantiate()
+		newCoal.transform = transform
+		newCoal.apply_impulse(Vector2(LAUNCH_SPEED,0))
+		get_parent().add_child(newCoal)
