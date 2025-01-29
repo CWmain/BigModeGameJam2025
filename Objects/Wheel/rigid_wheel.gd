3366ae41852bc697a2ff75cc836f148768a1e209
CWmain <cameronwhite2478@gmail.com>
2025-01-29 18:55:12 +1100

    Added sparks to indicate when wheel is moving too fast

--- Objects/Wheel/rigid_wheel.gd
+++ Objects/Wheel/rigid_wheel.gd
@@ -6,19 +6,20 @@ extends RigidBody2D
 
 ## When this power is exceeded triggers a break
 @export var maxPower : int = 300
-
+@export var supplyMult: int = 20
 var canHold: bool = false
-
-## Used to prevent spinning the wheel the wrong way
-var oldRotation: float = -1
 
 var SUPPLY : int = 0
 var broken: bool = false
+
+@onready var sparks = $Sparks
 
 ## Bool to prevent double adding when passing 2 Pi rotation
 var canSupplyAgain: bool = true
 
 func _physics_process(_delta):
+	if broken:
+		return
 	if canHold and Input.is_action_pressed("Hold"):
 		var directionToMouse : Vector2 = to_global(position).direction_to(get_viewport().get_mouse_position())
 		var distanceToMouse : float = to_global(position).distance_to(get_viewport().get_mouse_position())
@@ -29,15 +30,13 @@ func _physics_process(_delta):
 	if Input.is_action_just_released("Hold"):
 		canHold = false
 
-	# Since we are in process, use a bool to ensure the emit only triggers once
-	if rotation > PI/2.0 and canSupplyAgain:
-		canSupplyAgain = false
-		SUPPLY += powerPerTurn
+	SUPPLY = max(SUPPLY, abs(int(angular_velocity))*supplyMult)
 	
-	# Once the otherside is reached, allow another supply trigger to occur
-	if rotation < -(PI/2.0):
-		canSupplyAgain = true
-		
+	if SUPPLY > maxPower - 100:
+		sparks.emitting = true
+	else:
+		sparks.emitting = false
+	
 	# If overworked breaks
 	if SUPPLY > maxPower:
 		fallOff()
@@ -45,11 +44,14 @@ func _physics_process(_delta):
 func _on_handle_physics_mouse_entered():
 	canHold = true
 
+func _on_handle_physics_mouse_exited():
+	if !Input.is_action_pressed("Hold"):
+		canHold = false
+
 func getSupply() -> int:
 	if broken:
 		return 0
-		
-	var toReturn: int = SUPPLY
+	var toReturn = SUPPLY
 	SUPPLY = 0
 	return toReturn
 
