be8110a2ff1ef149a08892f483e7dd3633816440
CWmain <cameronwhite2478@gmail.com>
2025-02-01 12:52:16 +1100

    Created the stubs for main menu

---
+++ Levels/main_menu.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+
+func _on_start_pressed():
+	print("Go to load screen to background load level / particles")
+
+
+func _on_settings_pressed():
+	print("Hide menu options and show settings options")
