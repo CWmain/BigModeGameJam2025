ab2d45a561442f476ff61dde4415ba91ce034c42
CWmain <cameronwhite2478@gmail.com>
2025-02-01 13:03:00 +1100

    Have start button take player to load screen, which will load the game and pre-render all effects

--- Levels/main_menu.gd
+++ Levels/main_menu.gd
@@ -1,9 +1,10 @@
 extends Node2D
 
+var loadScreen = preload("res://Levels/load_screen.tscn")
 
 func _on_start_pressed():
 	print("Go to load screen to background load level / particles")
-
+	get_tree().change_scene_to_packed(loadScreen)
 
 func _on_settings_pressed():
 	print("Hide menu options and show settings options")
