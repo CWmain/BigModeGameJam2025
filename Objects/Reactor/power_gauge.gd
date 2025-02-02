c64939bf8b0a9d3d983554c0a6938f3c3740ba10
CWmain <cameronwhite2478@gmail.com>
2025-02-02 23:07:25 +1100

    Added glass breaking sound to reactor break

--- Objects/Reactor/power_gauge.gd
+++ Objects/Reactor/power_gauge.gd
@@ -5,11 +5,13 @@ extends Node2D
 @onready var constant_explosion = $ConstantExplosion
 
 @onready var flow = $Flow
+@onready var explosion = $Explosion
 
 func setGauge(percent: float) -> void:
 	assert(percent >= 0.0 and percent <= 1.0)
 	power_gauge.material.set_shader_parameter("percentFull", percent)
 
 func breakGauge() -> void:
+	explosion.play()
 	constant_explosion.emitting = true
 	flow.play()
