a068701bc48fec70e773548a1035953a51352783
CWmain <cameronwhite2478@gmail.com>
2025-02-02 23:03:21 +1100

    Added flowing water sound for when the reactor breaks, added stub for glass breaking sound to be added later

--- Objects/Reactor/power_gauge.gd
+++ Objects/Reactor/power_gauge.gd
@@ -4,9 +4,12 @@ extends Node2D
 
 @onready var constant_explosion = $ConstantExplosion
 
+@onready var flow = $Flow
+
 func setGauge(percent: float) -> void:
 	assert(percent >= 0.0 and percent <= 1.0)
 	power_gauge.material.set_shader_parameter("percentFull", percent)
 
 func breakGauge() -> void:
 	constant_explosion.emitting = true
+	flow.play()
